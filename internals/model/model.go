eb0cbedf4ea5689cd5a6dfc1cf02911c7908235e
Rommel Paras <rpparas@gmail.com>
2022-07-16 21:45:13 -0700

    Update data model to force unique values in DB

--- internals/model/model.go
+++ internals/model/model.go
@@ -9,18 +9,17 @@ import (
 
 type Robot struct {
 	gorm.Model
-	ID         uuid.UUID `json:"robotId" gorm:"primaryKey;type:uuid"`
-	Name       string    `json:"name" gorm:"type:varchar(100);unique_index"`
+	ID         uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	Name       string    `json:"name" gorm:"type:varchar(100);uniqueIndex"`
 	Generation uint      `json:"generation" gorm:"type:smallint"`
 }
 
 type Flight struct {
 	gorm.Model
-	ID uuid.UUID `json:"flightId" gorm:"primaryKey; type:uuid"`
-	// RobotID   uuid.UUID
-	// Robot     Robot     `json:"robot" gorm:"referencesRobotID"`
-	StartTime time.Time `json:"startTime"`
-	EndTime   time.Time `json:"endTime"`
-	Lat       float64   `json:"lat" gorm:"type:decimal(10,8)"`
-	Lng       float64   `json:"lng" gorm:"type:decimal(11,8)"`
+	ID        uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	RobotID   uuid.UUID `json:"robotId" gorm:"type:uuid REFERENCES robots(ID);uniqueIndex:idx_flight"`
+	StartTime time.Time `json:"startTime" gorm:"uniqueIndex:idx_flight"`
+	EndTime   time.Time `json:"endTime" gorm:"uniqueIndex:idx_flight"`
+	Lat       float64   `json:"lat" gorm:"type:decimal(10,8);uniqueIndex:idx_flight"`
+	Lng       float64   `json:"lng" gorm:"type:decimal(11,8);uniqueIndex:idx_flight"`
 }
