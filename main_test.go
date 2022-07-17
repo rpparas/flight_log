5d7b74b1d94a0bba29e06145ceb717d4031a35f2
Rommel Paras <rpparas@gmail.com>
2022-07-17 16:38:17 -0700

    Add test for /flights route

--- main_test.go
+++ main_test.go
@@ -5,14 +5,11 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/rpparas/flight_log/database"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIndexRoute(t *testing.T) {
-	// Define a structure for specifying input and output
-	// data of a single test case. This structure is then used
-	// to create a so called test map, which contains all test
-	// cases, that should be run for testing this function
+func T11estIndexRoute(t *testing.T) {
 	tests := []struct {
 		description string
 
@@ -39,6 +36,71 @@ func TestIndexRoute(t *testing.T) {
 			expectedBody:  "Cannot GET /i-dont-exist",
 		},
 	}
+
+	// Setup the app as it is done in the main function
+	app := Setup()
+
+	// Iterate through test single test cases
+	for _, test := range tests {
+		// Create a new http request with the route
+		// from the test case
+		req, _ := http.NewRequest(
+			"GET",
+			test.route,
+			nil,
+		)
+
+		// Perform the request plain with the app.
+		// The -1 disables request latency.
+		res, err := app.Test(req, -1)
+
+		// verify that no error occured, that is not expected
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+		// As expected errors lead to broken responses, the next
+		// test case needs to be processed
+		if test.expectedError {
+			continue
+		}
+
+		// Verify if the status code is as expected
+		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
+		// Read the response body
+		body, err := ioutil.ReadAll(res.Body)
+
+		// Reading the response body should work everytime, such that
+		// the err variable should be nil
+		assert.Nilf(t, err, test.description)
+
+		// Verify, that the reponse body equals the expected body
+		assert.Equalf(t, test.expectedBody, string(body), test.description)
+	}
+}
+
+func TestGetFlights(t *testing.T) {
+	tests := []struct {
+		description string
+
+		// Test input
+		route string
+
+		// Expected output
+		expectedError bool
+		expectedCode  int
+		expectedBody  string
+	}{
+		{
+			description:   "GET Flights route",
+			route:         "/api/v1/flights",
+			expectedError: false,
+			expectedCode:  200,
+			expectedBody:  "TODO: Add documentation how to use Drone Log API",
+		},
+	}
+
+	// Connect to the Database
+	database.ConnectDB()
 
 	// Setup the app as it is done in the main function
 	app := Setup()
