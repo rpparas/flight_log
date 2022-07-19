0d498d90724736313dce9400c136740a4f4872f9
Rommel Paras <rpparas@gmail.com>
2022-07-18 21:32:56 -0700

    Create tests for post requests

---
+++ main_post_request_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/rpparas/flight_log/database"
+)
+
+func TestPostFlights(t *testing.T) {
+	tests := []TestCase{
+		{
+			description: "POST single Flight",
+			route:       "/api/v1/flights",
+			payload: `{
+				"robotId": "e570b6c0-9bb0-47c9-a358-b984ed402406",
+				"startTime": "2022-07-18T01:00:01+00:00"
+			}`,
+			expectedError:   false,
+			expectedCode:    201,
+			expectedMessage: "Created Flight",
+		},
+		{
+			description: "POST duplicate Flight",
+			route:       "/api/v1/flights",
+			payload: `{
+				"robotId": "e570b6c0-9bb0-47c9-a358-b984ed402406",
+				"startTime": "2022-07-18T01:00:01+00:00"
+			}`,
+			expectedError:   false,
+			expectedCode:    500,
+			expectedMessage: "Could not create flight",
+		},
+	}
+	executePostTests(t, tests)
+}
+
+func executePostTests(t *testing.T, tests []TestCase) {
+	database.ConnectDB()
+
+	// Setup the app as it is done in the main function
+	app := Setup()
+
+	for _, test := range tests {
+		req, _ := http.NewRequest(
+			"POST",
+			test.route,
+			strings.NewReader(test.payload),
+		)
+
+		req.Header.Add("Content-Type", "application/json")
+		compareTestResults(t, test, app, req)
+	}
+
+}
--- test_definitions.go
+++ test_definitions.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+)
+
 type Result struct {
 	Message string
 }
@@ -13,4 +24,37 @@ type TestCase struct {
 	expectedError   bool
 	expectedCode    int
 	expectedMessage string
+}
+
+func compareTestResults(t *testing.T, test TestCase, app *fiber.App, req *http.Request) {
+	// verify that no error occured, that is not expected
+	res, err := app.Test(req, -1)
+	assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+	// As expected errors lead to broken responses, the next
+	// test case needs to be processed
+	if test.expectedError {
+		return
+	}
+
+	// Verify if the status code is as expected
+	assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
+	// Read the response body
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// parse api result as JSON
+	var apiResult Result
+	err = json.Unmarshal(body, &apiResult)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.Nilf(t, err, test.description)
+
+	assert.Equalf(t, test.expectedMessage, apiResult.Message, test.description)
+
 }
