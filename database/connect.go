89144a737e39085565b54879c1126f0e6420d12d
Rommel Paras <rpparas@gmail.com>
2022-07-20 19:52:54 -0700

    Update env variables used

--- database/connect.go
+++ database/connect.go
@@ -16,7 +16,7 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
-	p := config.Config("DB_PORT")
+	p := config.Config("POSTGRES_PORT")
 	port, err := strconv.ParseUint(strings.TrimSpace(p), 10, 32)
 
 	if err != nil {
@@ -24,7 +24,7 @@ func ConnectDB() {
 	}
 
 	// Connection URL to connect to Postgres Database
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("POSTGRES_HOST"), port, config.Config("POSTGRES_USER"), config.Config("POSTGRES_PASSWORD"), config.Config("POSTGRES_DB"))
 	// Connect to the DB and initialize the DB variable
 	DB, err = gorm.Open(postgres.Open(dsn))
 
